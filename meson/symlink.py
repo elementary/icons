adf21c17afb23cd294cba4c3aaa8ae12fc89e395
Corentin Noël <corentin@elementary.io>
2018-05-16 12:10:50 +0100

    Switch from CMake to Meson (#436)

---
+++ meson/symlink.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Create a symlink')
+parser.add_argument('--src', nargs=1)
+parser.add_argument('--dest', nargs=1)
+args = parser.parse_args()
+
+prefix = os.environ['MESON_INSTALL_DESTDIR_PREFIX']
+src = os.path.join(prefix, args.src[0])
+dest = os.path.join(prefix, args.dest[0])
+
+if os.path.exists(dest):
+    if os.path.isdir(dest):
+        print ('Folder "' + dest + '" already exists, no symlink will be created')
+    else:
+        print ('File "' + dest + '" already exists, no symlink will be created')
+else:
+    print ('Linking ' + dest + ' to ' + src)
+    os.symlink(src, dest)
