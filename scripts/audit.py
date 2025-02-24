ac844364c6ba91498dd63fb31ec23837a7066d1f
William Kelso <wkelso02@gmail.com>
2025-02-24 14:17:14 -0500

    Clean up remote pull and use argparse like a civilized script

--- scripts/audit.py
+++ scripts/audit.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 
-import os, csv
-from sys import argv
+import os, csv, argparse
 
 ignoreList = []
 
-if argv[1] == "-h":
-    print("USAGE: python (-O) audit.py <path-to-icon-spec-file> <path-to-root-dir-of-theme> (<report-file-name>)")
-    exit()
+parser = argparse.ArgumentParser(
+    prog = "audit",
+    description = "Audit a FreeDesktop.org compatible icon set against a specification"
+)
+parser.add_argument(
+    "themePath",
+)
+parser.add_argument(
+    "-s",
+    "--specification",
+    dest="specf"
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true"
+)
+parser.add_argument(
+    "-r",
+    "--report-name",
+    dest="reportf",
+    default="report"
+)
+args = parser.parse_args()
 
 # make hrules pretty
 try:
     viewWidth = os.get_terminal_size().columns
 except:
     viewWidth = 80
-
-try:
-    outputFName = argv[4]
-except:
-    outputFName = "report.csv"
 
 # Check for optional dependencies to enable drawing the spec from the web.
 # Otherwise fall back to included file
@@ -32,7 +47,7 @@ else:
     print("Found bs4 and requests dependencies, pulling spec from the web")
     foundDeps = True
 
-def get_soup(url: string) -> BeautifulSoup:
+def get_soup(url: str) -> BeautifulSoup:
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
     return soup
@@ -51,13 +66,29 @@ def parse_soup(soup: BeautifulSoup) -> list:
             name = text[0].text.strip().replace("\n", "")
             # skip the header row
             if name == "Name": continue
-            result.append(item)
+            result.append(name)
 
     return result
 
+def get_iso_3166() -> list:
+    result = []
+    page = requests.get("https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2")
+    soup = BeautifulSoup(page.text, "html.parser")
+    table = soup.find_all("table", class_="wikitable")[4].tbody
+    rows = table.find_all("tr")
+    for row in rows:
+        code = row.text.strip().replace("\n", " ").split()[0]
+        if code == "Code":
+            continue
+        result.append(f"flag-{code}")
+
+    return result
+        
+
+
 def padList(iterable: list, length: int) -> list:
     extensionLength = max(length - len(iterable), 0)
-    if __debug__: print(f"Extending list by {extensionLength} elements")
+    if args.verbose: print(f"Extending list by {extensionLength} elements")
     for _ in range(extensionLength):
         iterable.append(None)
 
@@ -70,30 +101,31 @@ def populateFromDir(dir: os.DirEntry) -> set:
     for entry in os.scandir(dir.path):
         if entry.name not in ignoreList:
             if entry.is_dir():
-                if __debug__: print(f"Found dir at {entry.path}, going deeper")
+                if args.verbose: print(f"Found dir at {entry.path}, going deeper")
                 # A spoonful of recursion to help the medicine go down
                 tree.extend(populateFromDir(entry))
             else:
-                if __debug__: print(f"Adding file {entry.path}, named {entry.name} to SubTree")
+                if args.verbose: print(f"Adding file {entry.path}, named {entry.name} to SubTree")
                 tree.append(entry.name.removesuffix(".svg"))
 
-    if __debug__: print(f"The SubTree for Dir {dir.name} is {tree}")
+    if args.verbose: print(f"The SubTree for Dir {dir.name} is {tree}")
     return list(set(tree))
 
 # Load a text stream of the spec into a dictionary, where KEY=icon_name &
 # VALUE=a bool denoting whether the file was found in the file tree
 # Iterate over the list, checking to see if the specified file can be found.
-fName = argv[1]
 specification = {}
 
 if foundDeps:
-    url = ""
+    url = "https://specifications.freedesktop.org/icon-naming-spec/latest/"
     soup = get_soup(url)
     specList = parse_soup(soup)
-    specification.fromkeys(specList, False)
+    specList.extend(get_iso_3166())
+    specList.remove("flag-aa")
+    specification = specification.fromkeys(specList, False)
 else:
-    with open(fName, "r") as file:
-        print(f"Loading specification in file: {fName}...")
+    with open(args.specf, "r") as file:
+        print(f"Loading specification in file: {specf}...")
         for line in file:
             if line.startswith(("//", " ", "\n")):
                 continue
@@ -107,7 +139,7 @@ symbolicSpecification = specification.copy()
 # a list. Anything in this list doesn't exist as far as this script is
 # concerned.
 try:
-    targetDir = argv.pop(2)
+    targetDir = args.themePath 
 except:
     print("Please specify a directory")
     exit()
@@ -126,7 +158,7 @@ except:
     print("No .auditignore file found in root")
 else:
     print("Loaded .auditignore file!")
-    if __debug__: print(f"Values contained: {ignoreList}")
+    if args.verbose: print(f"Values contained: {ignoreList}")
 
 print('-' * viewWidth)
 
@@ -136,18 +168,18 @@ contents = []
 for entry in os.scandir():
     if entry.name not in ignoreList:
         if entry.is_dir():
-            if __debug__: print(f"Found dir at {entry.path}")
+            if args.verbose: print(f"Found dir at {entry.path}")
             subDirTree = populateFromDir(entry)
             contents.extend(subDirTree)
         else:
-            if __debug__: print(f"Adding file {entry.path} to tree")
+            if args.verbose: print(f"Adding file {entry.path} to tree")
             if entry.name.endswith(".svg"):
                 contents.append(entry.name.removesuffix(".svg"))
 
 # remove duplicate names from the list
 contents = list(set(contents))
 
-if __debug__:
+if args.verbose:
     print(f"The full dirTree is {contents}")
     print('-' * viewWidth)
 
@@ -213,17 +245,17 @@ for i, result in enumerate(colorResults):
     # If the result is false, then it is missing
     name = specList[i]
     if result:
-        if __debug__: print(f"Adding {name} to Found...")
+        if args.verbose: print(f"Adding {name} to Found...")
         foundEntries.append(name)
     else:
-        if __debug__: print(f"Adding {name} to Missing...")
+        if args.verbose: print(f"Adding {name} to Missing...")
         missingEntries.append(name)
 
 # Now we want to go through all of the entries we found in the initial contents and
 # point out those that aren't in the spec
 for entry in contents:
     if entry not in specList and not entry.endswith("-symbolic"):
-        if __debug__: print(f"Adding {entry} to Out of Spec")
+        if args.verbose: print(f"Adding {entry} to Out of Spec")
         outOfSpecEntries.append(entry)
 
 # Pad all lists to be the same length, then zip all three lists together
@@ -236,9 +268,9 @@ outOfSpecEntries = padList(outOfSpecEntries, length)
 zippedLists = [(a, b, c) for a, b, c in zip(foundEntries, missingEntries,
                                             outOfSpecEntries)]
 
-with open(outputFName, 'w', newline='') as file:
+with open(args.reportf, 'w', newline='') as file:
     fWriter = csv.writer(file)
     fWriter.writerow(["Found", "Missing", "Out of Spec"])
     fWriter.writerows(zippedLists)
-    print(f"Report written to {os.getcwd()}/{outputFName}")
+    print(f"Report written to {os.getcwd()}/{args.reportf}")
 
